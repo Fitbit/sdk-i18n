31aebd9aa952763195d6c95042bc344e85cd5efa
Jon Barlow <jon.barlow@fitbit.com>
2019-03-12 01:21:28 +0000

    Added some real localizations, updated SDK version

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
-import document from 'document';
-import { gettext } from 'i18n';
+import document from "document";
+import { gettext } from "i18n";
 
-const demoText = document.getElementById('#demotext');
-
-demoText.text = gettext('hello_world');
+const demoText = document.getElementById("#demotext");
+demoText.text = `100 ${gettext("Active Minutes")}`;
